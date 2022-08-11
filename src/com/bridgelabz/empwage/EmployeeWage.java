5f81853f6dec2a15bb61ef952dcfbdd26e0fe5e1
shekharJha00 <99266926+shekharJha00@users.noreply.github.com>
2022-08-11 09:05:07 +0530

    Revert "Uc5"

--- src/com/bridgelabz/empwage/EmployeeWage.java
+++ src/com/bridgelabz/empwage/EmployeeWage.java
@@ -7,31 +7,23 @@ public class EmployeeWage {
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        final int WORKING_DAYS = 20;
-
-        int totalWage = 0;
-        for (int day = 1; day <= WORKING_DAYS; day++)
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours =0;
+        switch (empType)
         {
-            int empType = (int) (Math.random() * 100) % 3;
-            int workingHours =0;
-            switch (empType)
-            {
-                case FULL_TIME:
-                    System.out.println("Emplyoee is present full-time");
-                    workingHours = 8;
-                    break;
-                case PART_TIME:
-                    System.out.println("Employee is present Part-time");
-                    workingHours = 4;
-                    break;
-                default:
-                    System.out.println("Employee is Absent");
-            }
-            int wage = workingHours * WAGE_PER_HR;
-            System.out.println("Day " + day + " wage is:" + wage);
-            totalWage += wage;
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
-        System.out.println("Total wage for a month is " + totalWage);
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
 
